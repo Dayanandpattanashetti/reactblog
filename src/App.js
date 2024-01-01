f575b1e754fe9ee563ae3ea18eeab0ceb51f8657
Dayanandpattanashetti <dayanandweb@gmail.com>
2024-01-01 16:01:20 +0530

    App.js changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Topbar from './components/topbar/Topbar';
-import Home from './pages/home/Home';
-import Single from './pages/single/Single';
-import Write from './pages/write/Write';
-import Settings from './pages/settings/Settings';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Context } from './context/Context';
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Context } from "./context/Context";
 
 function App() {
   const { user } = useContext(Context);
@@ -16,13 +16,13 @@ function App() {
       <Topbar />
 
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/posts' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/post/:id' element={<Single />} />
-        <Route path='/write' element={user ? <Write /> : <Login />} />
-        <Route path='/settings' element={user ? <Settings /> : <Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/post/:id" element={<Single />} />
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Login />} />
       </Routes>
     </Router>
   );
